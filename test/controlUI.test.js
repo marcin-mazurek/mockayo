2437353f4c0a7e817ab4bcbd36d877e607026070
Marcin Mazurek <marcin@mazurek.pro>
2019-02-16 19:18:02 +0100

    Fix test stability

--- test/controlUI.test.js
+++ test/controlUI.test.js
@@ -14,29 +14,35 @@ test("launches Control UI", async t => {
   t.is(expectedPageContent, pageContent);
 });
 
-test("exposes endpoints available in configuration through GET /__admin/get-endpoints", async t => {
-  const response = await fetch("http://localhost:5000/__admin/get-endpoints");
-  const endpoints = await response.json();
+test.serial(
+  "exposes endpoints available in configuration through GET /__admin/get-endpoints",
+  async t => {
+    const response = await fetch("http://localhost:5000/__admin/get-endpoints");
+    const endpoints = await response.json();
 
-  t.snapshot(endpoints);
-});
+    t.snapshot(endpoints);
+  }
+);
 
-test("remembers the scenario set with POST /__admin/set-scenario", async t => {
-  await fetch("http://localhost:5000/__admin/set-scenario", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      file: "error.js",
-      url: "/users",
-      method: "GET"
-    })
-  });
+test.serial(
+  "remembers the scenario set with POST /__admin/set-scenario",
+  async t => {
+    await fetch("http://localhost:5000/__admin/set-scenario", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        file: "error.js",
+        url: "/users",
+        method: "GET"
+      })
+    });
 
-  const endpointResponse = await fetch("http://localhost:5000/users");
-  const endpointResponseBody = await endpointResponse.json();
+    const endpointResponse = await fetch("http://localhost:5000/users");
+    const endpointResponseBody = await endpointResponse.text();
 
-  t.is(endpointResponse.status, 500);
-  t.deepEqual(endpointResponseBody, { message: "Something went wrong!" });
-});
+    t.is(endpointResponse.status, 500);
+    t.is(endpointResponseBody, "Something went wrong!");
+  }
+);
--- test/testFiles/mocks/get-users/error.js
+++ test/testFiles/mocks/get-users/error.js
@@ -1,4 +1,4 @@
 module.exports = {
   code: 500,
-  body: { message: "Something went wrong!" }
+  body: "Something went wrong!"
 };
